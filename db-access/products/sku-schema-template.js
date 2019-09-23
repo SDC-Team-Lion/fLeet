13d6745c043aa9a2735a99b9cb536c081666f186
Jared A Sutton <jaredasutton@Jareds-MacBook-Pro.local>
2019-09-23 12:27:02 -0400

    Write out schema for sku aggregation.

--- db-access/products/sku-schema-template.js
+++ db-access/products/sku-schema-template.js
@@ -6,20 +6,11 @@ db.products.aggregate([
   {
     $lookup: {
       from: "styles",
-      "let": {"style_id":"$style_id"},
+      "let": {"styleIdForStyle":"$style_id"},
       pipeline: [
         { $match: { productId: 1 } },
         { $project: { _id: 0, __v: 0, productId: 0 } },
-        {
-          $lookup: {
-            from: "skus",
-            pipeline: [
-              { $match: { styleId: "$style_id" } },
-              { $project: { size: 1, quantity: 1, style_id: 1 } }
-            ],
-            as: "skus"
-          }
-        }
+        
       ],
       as: "results"
     }
@@ -28,7 +19,17 @@ db.products.aggregate([
 */
 
 //from: "styles",localField:"styleId",foreignField:"style_id",as:"skus"
-
+// {
+//   $lookup: {
+//     from: "skus",
+//     let: {"styleIdForSku":"$styleId"},
+//     pipeline: [
+//       { $match: { "$expr": { "$eq": ["$styleIdForStyle", "$$styleIdForSku"] }} },
+//       { $project: { size: 1, quantity: 1, style_id: 1 } }
+//     ],
+//     as: "skus"
+//   }
+// }
 /*
 db.products.aggregate([
   { $match: { id: 1 } },
@@ -42,6 +43,77 @@ db.products.aggregate([
         {
           $lookup: {
 from: "styles",localField:"styleId",foreignField:"style_id",as:"skus"
+          }
+        }
+      ],
+      as: "results"
+    }
+  }
+]);
+*/
+/*  //This will aggregate styles
+db.styles.aggregate([
+  { $match: { productId: 1 } },
+  { $project: { _id: 0, __v: 0 } },
+  {
+    $lookup: {
+      from: "skus",
+      let: { styleIdForStyle: "$style_id" },
+      pipeline: [
+        { $match: { $expr: { $eq: ["$$styleIdForStyle", "$styleId"] } } },
+        { $project: { _id: 0, __v: 0 } }
+      ],
+      as: "results"
+    }
+  }
+]);
+*/
+/* // This will aggregate skus under styles and styles under products
+db.products.aggregate([
+  { $match: { id: 1 } },
+  { $project: { _id: 0, __v: 0 } },
+  {
+    $lookup: {
+      from: "styles",
+      pipeline: [
+        { $match: { productId: 1 } },
+        { $project: { _id: 0, __v: 0, productId: 0 } },
+        {
+          $lookup: {
+            from: "skus",
+      let: { styleIdForStyle: "$style_id" },
+      pipeline: [
+        { $match: { $expr: { $eq: ["$$styleIdForStyle", "$styleId"] } } },
+        { $project: { _id: 0, __v: 0 } }
+      ],
+      as: "skus"
+          }
+        }
+      ],
+      as: "results"
+    }
+  }
+]);
+*/
+/*
+db.products.aggregate([
+  { $match: { id: 1 } },
+  { $project: { _id: 0, __v: 0 } },
+  {
+    $lookup: {
+      from: "styles",
+      pipeline: [
+        { $match: { productId: 1 } },
+        { $project: { _id: 0, __v: 0, productId: 0 } },
+        {
+          $lookup: {
+            from: "skus",
+            let: { styleIdForStyle: "$style_id" },
+            pipeline: [
+              { $match: { $expr: { $eq: ["$$styleIdForStyle", "$styleId"] } } },
+              { $project: { _id: 0, __v: 0 } }
+            ],
+            as: "skus"
           }
         }
       ],
