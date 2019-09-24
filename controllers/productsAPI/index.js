86d83b892031d3f6ee29c4e52881257b7d06f2b9
Jared A Sutton <jaredasutton@Jareds-MacBook-Pro.local>
2019-09-24 11:30:45 -0400

    Integrate aggregate query for related products with route.

--- controllers/productsAPI/index.js
+++ controllers/productsAPI/index.js
@@ -1,2 +1,3 @@
 exports.products = require("./products.js");
 exports.styles = require("./styles.js");
+exports.relateds = require("./relateds.js");
--- routes/productsAPI/index.js
+++ routes/productsAPI/index.js
@@ -1,8 +1,13 @@
 const router = require("express").Router();
-const { products, styles } = require("../../controllers/productsAPI/index.js");
+const {
+  products,
+  styles,
+  relateds
+} = require("../../controllers/productsAPI/index.js");
 
 router.get("/list", products.getMany);
 router.get("/:product_id/styles", styles.getAllForProductId);
+router.get("/:product_id/related", relateds.getAllForProductId);
 router.get("/:product_id", products.getOneById);
 
 module.exports = router;
