7f9093a65615b18fd2050058cc808c0e72d9f7ab
Dan Sha <danyun.sha@gmail.com>
2019-09-28 11:15:37 -0400

    Separate post formatting into helpers.js

--- models/reviewsAPI/Reviews.js
+++ models/reviewsAPI/Reviews.js
@@ -30,40 +30,13 @@ module.exports = {
   },
 
   postReview: (revObj, prod_id, callback) => {
-    let today = new Date();
-    let dd = String(today.getDate()).padStart(2, '0');
-    let mm = String(today.getMonth() + 1).padStart(2, '0');
-    let yyyy = today.getFullYear();
-    today = `${mm}/${dd}/${yyyy}`;
-    let charQuery = '';
-    let photoQuery = `
-      INSERT INTO photos (id,review_id,url_str)
-      VALUES (DEFAULT,`;
-    // for (let i = 0; i < revObj.photos.length; i++) {
-    //   photoQuery +=
-    // }
-
-    let query = `
-      INSERT INTO reviews
-      (id,product_id,rating,rev_date,summary,body,recommend,reported,reviewer_name,reviewer_email,response,helpfulness)
-      VALUES (DEFAULT,${prod_id},${revObj.rating},${today},${revObj.summary},${revObj.body},${revObj.recommended},false,${revObj.name},${revObj.email},null,0);
-      ${charQuery}
-      ${photoQuery}`;
+    let query = format.newReview(revObj, prod_id);
 
     pgQuery.post(query, (err, results) => {
       callback(err, results);
     });
   },
 
-  // let submission = {
-  //   rating: rating,
-  //   summary: this.state.summaryEntry,
-  //   body: this.state.bodyEntry,
-  //   recommended: (this.state.rec === "yes") ? "true" : "false",
-  //   name: this.state.nameEntry,
-  //   email: this.state.emailEntry,
-  //   photos: this.state.photos, array of URL strings
-  //   characteristics: this.state.characteristics
   getCount: (callback) => {
     let query = `select count(*) from reviews;`;
     pgQuery.get(query, (err, results) => {
--- models/reviewsAPI/format.js
+++ models/reviewsAPI/format.js
@@ -26,7 +26,40 @@ module.exports = {
       }
       result["results"].push(tracker);
     }
-
     return result;
+  },
+  newReview: (revObj, prod_id) => {
+
+    let charQuery = '';
+    let photoQuery = `
+      INSERT INTO photos (id,review_id,url_str)
+      VALUES (DEFAULT,`;
+    // for (let i = 0; i < revObj.photos.length; i++) {
+    //   photoQuery +=
+    // }
+
+    let today = new Date();
+    let dd = String(today.getDate()).padStart(2, '0');
+    let mm = String(today.getMonth() + 1).padStart(2, '0');
+    let yyyy = today.getFullYear();
+    today = `${mm}/${dd}/${yyyy}`;
+
+    let query = `
+    INSERT INTO reviews
+    (id,product_id,rating,rev_date,summary,body,recommend,reported,reviewer_name,reviewer_email,response,helpfulness)
+    VALUES (DEFAULT,${prod_id},${revObj.rating},${today},${revObj.summary},${revObj.body},${revObj.recommended},false,${revObj.name},${revObj.email},null,0);
+    ${charQuery}
+    ${photoQuery}`;
+
+    return query;
   }
+    // let submission = {
+  //   rating: rating,
+  //   summary: this.state.summaryEntry,
+  //   body: this.state.bodyEntry,
+  //   recommended: (this.state.rec === "yes") ? "true" : "false",
+  //   name: this.state.nameEntry,
+  //   email: this.state.emailEntry,
+  //   photos: this.state.photos, array of URL strings
+  //   characteristics: this.state.characteristics
 }
